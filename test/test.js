80dbb7220b966878064ca32b5af88d23765d90a6
Iggy Grey <github@pmphillips.ca>
2018-11-22 12:31:38 -0800

    Changed test

--- test/test.js
+++ test/test.js
@@ -175,14 +175,6 @@ contract('Trust - Deploying and storing all contracts + validation', async (acco
 
    it ('Make sure Trust contract is destroyed', async() => {
      let err;
-     try {await trust.changeExpiration(0, {from: trustor});}
-     catch(e) {
-         err = e;
-     }
-     assert.notEqual(err, undefined);
-
-     // Try Withdrawing
-     err = undefined;
      try { await trust.withdraw({from: beneficiary});  }
      catch(e) {
          console.log("EVM error: No income left in trust");
